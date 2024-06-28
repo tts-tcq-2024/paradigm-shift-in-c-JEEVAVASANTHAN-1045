cafc69f037a29b02a51041bad3118047fa0d81be
JEEVAVASANTHAN-1045 <jiivavasanthan@gmail.com>
2024-06-28 09:43:19 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,46 +1,24 @@
 #include <stdio.h>
 #include <assert.h>
- 
+
 #define OUTOFBOUNDARY 0
 #define WITHINRANGE 1
- 
-/*Function to check whether the battery paramter is within the range*/
-int checkRange(float data, float min, float max, const char* nameOfBatteryParameter)
-{
-int output = WITHINRANGE;
-if (data > max || data < min)
-{
-  output = OUTOFBOUNDARY;
-  logger(nameOfBatteryParameter);
+
+int checkRange(float data, float min, float max, const char* nameOfBatteryParameter) {
+ if (data > max || data < min) {
+     printf("%s out of range!\n", nameOfBatteryParameter);
+     return OUTOFBOUNDARY;
+ }
+ return WITHINRANGE;
 }
-return output;
+
+int batteryIsOk(float temperature, float soc, float chargeRate) {
+ return checkRange(temperature, 0, 45, "Temperature") &&
+        checkRange(soc, 20, 80, "State of Charge") &&
+        checkRange(chargeRate, 0, 0.8, "Charge Rate");
 }
- 
-/*Function to print the error logs*/
-void logger(const char* stringToBePrinted)
-{
-printf("%s out of range!\n",stringToBePrinted);
-}
- 
-int batteryIsOk(float temperature, float soc, float chargeRate) 
-{
-int functionResult = 1; 
-if(checkRange(temperature,0,45,"Temperature") == OUTOFBOUNDARY)
-{
-  functionResult = 0;
-}
-else if(checkRange(soc,20,80,"State of Charge") == OUTOFBOUNDARY)
-{
-  functionResult = 0;
-}
-else
-{
-  functionResult = checkRange(chargeRate,0,0.8,"Charge Rate");
-}
-  return functionResult;
-}
- 
+
 int main() {
-  assert(batteryIsOk(25, 70, 0.7));
-  assert(!batteryIsOk(50, 85, 0));
+ assert(batteryIsOk(25, 70, 0.7));
+ assert(!batteryIsOk(50, 85, 0));
 }
