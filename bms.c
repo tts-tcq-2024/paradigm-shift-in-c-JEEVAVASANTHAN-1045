bf41994318bcf0808358ed8c382eb2f70f94219b
JEEVAVASANTHAN-1045 <jiivavasanthan@gmail.com>
2024-08-05 10:20:37 +0530

    Create bms.c

---
+++ bms.c
@@ -0,0 +1,13 @@
+#ifndef TESTCASES_H
+#define TESTCASES_H
+
+void testTemperatureBelowMinimum();
+void testTemperatureAboveMaximum();
+void testSocBelowMinimum();
+void testSocAboveMaximum();
+void testChargeRateAboveMaximum();
+void testNearMinimumTemperatureLimit();
+void testNearMaximumTemperatureLimit();
+void testNearMinimumSocLimit();
+void testNearMaximumSocLimit();
+void testNearMaximumChargeRateLimit();
