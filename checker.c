eafcff7a740bf2830113884baebe66b6c9b98dde
JEEVAVASANTHAN-1045 <jiivavasanthan@gmail.com>
2024-08-05 09:30:10 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,24 +1,42 @@
 #include <stdio.h>
 #include <assert.h>
 
-#define OUTOFBOUNDARY 0
-#define WITHINRANGE 1
+typedef struct {
+    const char* name;
+    float min;
+    float max;
+    float value;
+} BatteryParameter;
 
-int checkRange(float data, float min, float max, const char* nameOfBatteryParameter) {
- if (data > max || data < min) {
-     printf("%s out of range!\n", nameOfBatteryParameter);
-     return OUTOFBOUNDARY;
- }
- return WITHINRANGE;
+#define OUT_OF_BOUNDARY 0
+#define WITHIN_RANGE 1
+
+int checkRange(BatteryParameter parameter) {
+    if (parameter.value < parameter.min || parameter.value > parameter.max) {
+        printf("%s out of range!\n", parameter.name);
+        return OUT_OF_BOUNDARY;
+    }
+    return WITHIN_RANGE;
 }
 
 int batteryIsOk(float temperature, float soc, float chargeRate) {
- return checkRange(temperature, 0, 45, "Temperature") &&
-        checkRange(soc, 20, 80, "State of Charge") &&
-        checkRange(chargeRate, 0, 0.8, "Charge Rate");
+    BatteryParameter parameters[] = {
+        {"Temperature", 0, 45, temperature},
+        {"State of Charge", 20, 80, soc},
+        {"Charge Rate", 0, 0.8, chargeRate}
+    };
+
+    for (int i = 0; i < sizeof(parameters) / sizeof(parameters[0]); ++i) {
+        if (checkRange(parameters[i]) == OUT_OF_BOUNDARY) {
+            return 0;
+        }
+    }
+    return 1;
 }
 
 int main() {
- assert(batteryIsOk(25, 70, 0.7));
- assert(!batteryIsOk(50, 85, 0));
+    assert(batteryIsOk(25, 70, 0.7));       // Expected: within range
+    assert(!batteryIsOk(50, 85, 0));        // Expected: out of range
+    printf("All tests passed!\n");
+    return 0;
 }
